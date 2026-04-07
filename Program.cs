b7caab566e46c64e542c9c4793944d211291b84c
Eigene Datei <waldemar65@hotmail.de>
2026-04-07 11:59:53 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Notepad.exe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
