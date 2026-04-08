d8c458cab69bb4dd0f9e29711e55a84786e829d3
Eigene Datei <waldemar65@hotmail.de>
2026-04-08 13:07:41 +0200

    #

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿namespace Notepad.exe
 {
-    internal class Program
+    internal class Program  39283773a2969fcfc26402b71ee10147f36bcc16
+        ADD DOCUMENT 
+  ’text’
+  ’text’.
     {
         static void Main(string[] args)
         {
